9b63767e88f2243719815fa543103681ce0350cc
Kamil Orwat <orwat.kamil3@gmail.com>
2023-02-08 14:19:25 +0100

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Container } from "./components/styles/Container.styled"
+import Header from "./components/Header"
+import Card from "./components/Card"
+import { ThemeProvider } from "styled-components"
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+import Footer from "./components/Footer"
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px'
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card item={item} key={index} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  )
+}
+
+export default App
